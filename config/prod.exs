cd80f702109ca7ad2a452060c862b52378298057
olafszad <olaf.szadkowski@gmail.com>
2022-12-22 14:18:56 +0100

    Fixed prod.exs - unnecessary code

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,6 @@ config :era, EraWeb.Endpoint,
     force_ssl: [rewrite_on: [:x_forwarded_proto]],
     cache_static_manifest: "priv/static/cache_manifest.json",
     secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
-    secret_key_base: Map.fetch!(System.get_env(), "DB_PASSWORD")
 
 # Do not print debug messages in production
 config :logger, level: :info
